6c48ac2a99530d501f4063b1282f140fc28b4e6a
alexandersharabayko <alexander.sharabayko@expcapital.com>
2018-10-01 10:23:42 +0300

    Add about page

---
+++ src/views/about/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <h2 class="page-title">
+      <span class="text">Информация</span>
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component} from "vue-property-decorator";
+
+  @Component
+  export default class About {
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
